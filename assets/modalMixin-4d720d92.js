4bb6e401a5ab41c3ac1f143d0d25e6d0b6c0856d
NoName135 <qscwdvefb3@gmail.com>
2023-02-20 20:01:32 +0800

    deploy

---
+++ assets/modalMixin-4d720d92.js
@@ -0,0 +1,1 @@
+import{M as o}from"./DeleteModal-41146326.js";const a={methods:{openModal(){this.modal.show()},hideModal(){this.modal.hide()}},mounted(){this.modal=new o(this.$refs.modal)}};export{a as m};
