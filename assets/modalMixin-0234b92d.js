1c495ea1935bd2b5598cb52932f8386023363dec
NoName135 <qscwdvefb3@gmail.com>
2023-02-20 20:54:21 +0800

    deploy

---
+++ assets/modalMixin-0234b92d.js
@@ -0,0 +1,1 @@
+import{M as o}from"./DeleteModal-3a18a4a2.js";const a={methods:{openModal(){this.modal.show()},hideModal(){this.modal.hide()}},mounted(){this.modal=new o(this.$refs.modal)}};export{a as m};
--- assets/modalMixin-4d720d92.js
+++
@@ -1,1 +0,0 @@
-import{M as o}from"./DeleteModal-41146326.js";const a={methods:{openModal(){this.modal.show()},hideModal(){this.modal.hide()}},mounted(){this.modal=new o(this.$refs.modal)}};export{a as m};
