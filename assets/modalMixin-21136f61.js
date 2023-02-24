441a41f55f96f6571f77db688148985f0522f2f7
NoName135 <qscwdvefb3@gmail.com>
2023-02-24 23:24:01 +0800

    deploy

---
+++ assets/modalMixin-21136f61.js
@@ -0,0 +1,1 @@
+import{M as o}from"./DeleteModal-df886aa5.js";const a={methods:{openModal(){this.modal.show()},hideModal(){this.modal.hide()}},mounted(){this.modal=new o(this.$refs.modal)}};export{a as m};
